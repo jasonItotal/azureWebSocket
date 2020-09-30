b8a585befeab5eeb8652b4c0de6f9ce32777bdb2
unknown <jason@JasonPC.i-Total.local>
2020-09-30 11:47:07 +0800

    test azure

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# azureWebSocket
+
+# Socket.IO Chat
+
+A simple chat demo for socket.io
+
+## How to use
+
+```
+$ cd socket.io
+$ npm install
+$ cd examples/chat
+$ npm install
+$ npm start
+```
+
+And point your browser to `http://localhost:3000`. Optionally, specify
+a port by supplying the `PORT` env variable.
+
+## Features
+
+- Multiple users can join a chat room by each entering a unique username
+on website load.
+- Users can type chat messages to the chat room.
+- A notification is sent to all users when a user joins or leaves
+the chatroom.
+
+## Setup Socket IO at azure
+- 1. download socket.io zip https://github.com/socketio/socket.io
+- 2. extract the example/chat to target folder
+- 3. modify app.js to index.js
+- 4. update app.js content
+- // var io = require('../..')(server);
+- // New:
+- var io = require('socket.io')(server);
+- 5. npm install socket.io
+- 6. npm install
+- 7. update package.json 
+- "main": "index.js" -> "main": "app.js"
+- "start": "node index.js" -> "start": "node app.js"
+- 
+- vs code part
+- 1. install azure app service ext
+- 2. login azure
+- 3. deploy to web app
+- 4. open azure port -> configuration, add application setting
+- 5. Key: WEBSITES_PORT, Value: 3000
---
+++ app.js
@@ -0,0 +1,146 @@
+// Setup basic express server
+const sql = require('mssql')
+var express = require('express')
+var app = express()
+var path = require('path')
+var axios = require('axios')
+var WEB_API = 'https://jobtotalwebapi.azurewebsites.net/api'
+if(process){
+  WEB_API = process.env.WEB_API
+}
+// var WEB_API = 'https://jobtotalwebapi.azurewebsites.net/api'
+var server = require('http').createServer(app)
+var io = require('socket.io')(server)
+// var port = process.env.PORT || 3000
+var port = 3000
+if(process){
+  port = process.env.PORT
+}
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port)
+})
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Chatroom
+
+var numUsers = 0
+
+sql.on('error', (err) => {
+  // ... error handler
+  console.dir(err)
+})
+
+io.on('connection', (socket) => {
+  io.set('transports', ['websocket'])
+  let localConfig = 'mssql://itotal:Zaq12wsx@152.101.178.178/JobTotalDev'
+  // let localConfig = '';
+  // var connectionString = process.env.SQLCONNSTR_JobTotalDev || localConfig
+  var connectionString = localConfig
+  if(process){
+    connectionString = process.env.SQLCONNSTR_JobTotalDev
+  }
+  // console.log('socket.handshake.query', socket.handshake.query)
+  console.log('connectionString', connectionString)
+
+  // var token = 'jason' //socket.handshake.query.token
+  // var chat_id = 1 //socket.handshake.query.chat_id
+  var token = socket.handshake.query.token
+  var chat_id = socket.handshake.query.chat_id
+
+
+  socket.join(chat_id)
+
+  var addedUser = false
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add_user', (chatToken) => {
+    if (addedUser) return
+
+    sql
+      .connect(connectionString)
+      .then((pool) => {
+        // Query
+        let queryString = 'select u.Name from JobTotalUser u, UserChat uc '
+        queryString += ' where u.deleted = 0 and uc.deleted = 0 and u.id = uc.user_id and '
+        queryString += ' u.token = @token and uc.chat_id = @chat_id and u.expiry > @expiry'
+        return pool.request()
+                    .input('token', sql.NVarChar, token)
+                    .input('chat_id', sql.BigInt, chat_id)
+                    .input('expiry', sql.DateTime, new Date())
+                    .query(queryString)
+      })
+      .then((result) => {
+        token = token
+        chat_id = chat_id
+        if (result.recordset.length > 0) {
+          var user = result.recordset[0]
+          console.log('chat: ', chat_id)
+          console.log('token: ', token)
+          console.log('user: ', user)
+
+          socket.username = user.Name
+          ++numUsers
+          addedUser = true
+          let data ={
+            username: socket.username,
+            numUsers: numUsers,
+          }
+          console.log('data',data)
+          socket.emit('login',data )
+          console.log('after emit login')
+          // echo globally (all clients) that a person has connected
+          socket.to(chat_id).emit('user_joined',data )
+          console.log('after user_joined', chat_id)
+        } else {
+          console.log('user chat not found with token, chat: ', token, chat_id)
+          // echo globally (all clients) that a person has connected
+          socket.to(chat_id).emit('connect_error', {
+            error: 'user/chat not found',
+          })
+        }
+      })
+      .catch((err) => {
+        console.log('err: ', err)
+      })
+  })
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.to(chat_id).emit('typing', {
+      username: socket.username,
+    })
+  })
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop_typing', () => {
+    socket.to(chat_id).emit('stop_typing', {
+      username: socket.username,
+    })
+  })
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new_message', (data) => {
+    console.log('chat_id', chat_id)
+    // we tell the client to execute 'new message'
+    socket.to(chat_id).emit('new_message', {
+      username: socket.username,
+      message: data.message,
+    })
+  })
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers
+
+      // echo globally that this client has left
+      socket.to(chat_id).emit('user_left', {
+        username: socket.username,
+        numUsers: numUsers,
+      })
+    }
+  })
+})
